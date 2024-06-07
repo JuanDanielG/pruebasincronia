af9cf8dce91413b91d954b6c5cbdea7013b762da
JuanDanielG <jdgaleanon@ufpso.edu.co>
2024-06-07 18:14:12 -0500

    prueba sincronia

---
+++ src/vue/LoadData.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {ref} from 'vue'
+
+const users = ref([])
+
+function loadData(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+     console.log(data)
+     users.value = data
+    })
+}
+</script>
+
+<template>
+    <div
+    v-for="users in users"
+    :key="users.id"
+    class="bg-slate-500 mb-1"
+    >
+    {{ users.name }}
+    </div>
+    <button @click="loadData">
+        Cargar info
+        </button>
+
+
+</template>
